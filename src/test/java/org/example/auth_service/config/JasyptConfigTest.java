0d0f73ae4abc3a2da93aaa0eb72d5b76234ceffb
tony <dohyun0047@gmail.com>
2025-02-10 22:08:31 +0900

    done: 1) add prod yaml

--- src/test/java/org/example/auth_service/config/JasyptConfigTest.java
+++ src/test/java/org/example/auth_service/config/JasyptConfigTest.java
@@ -3,30 +3,29 @@ package org.example.auth_service.config;
 import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class JasyptConfigTest {
-    @Test
-    void jasycrpt() {
-        String url = "jdbc:mysql://mysql-test:3316/test?serverTimezone=UTC&characterEncoding=UTF-8";
-        String username = "root";
-        String password = "1234";
-        String mail = "tonydevpc123@gmail.com";
-//        String mailpassword = "oirhttfuzgfsonyq";
-        String mailpassword = "udnyoanloarjiucu";
-        System.out.println(jasyptEncoding(url));
-        System.out.println(jasyptEncoding(username));
-        System.out.println(jasyptEncoding(password));
-        System.out.println(jasyptEncoding(mail));
-        System.out.println(jasyptEncoding(mailpassword));
-    }
 
-    public String jasyptEncoding(String value) {
-        String key = "my_jasypt_key";
-        StandardPBEStringEncryptor pbeEnc = new StandardPBEStringEncryptor();
-        pbeEnc.setAlgorithm("PBEWithMD5AndDES");
-        pbeEnc.setPassword(key);
-        return pbeEnc.encrypt(value);
-    }
+  @Test
+  void jasycrpt() {
+    String url = "jdbc:mysql://mysql-test:3306/test?serverTimezone=UTC&characterEncoding=UTF-8";
+    String username = "root";
+    String password = "1234";
+    String mail = "tonydevpc123@gmail.com";
+//        String mailpassword = "oirhttfuzgfsonyq";
+    String mailpassword = "udnyoanloarjiucu";
+    System.out.println(jasyptEncoding(url));
+    System.out.println(jasyptEncoding(username));
+    System.out.println(jasyptEncoding(password));
+    System.out.println(jasyptEncoding(mail));
+    System.out.println(jasyptEncoding(mailpassword));
+  }
+
+  public String jasyptEncoding(String value) {
+    String key = "my_jasypt_key";
+    StandardPBEStringEncryptor pbeEnc = new StandardPBEStringEncryptor();
+    pbeEnc.setAlgorithm("PBEWithMD5AndDES");
+    pbeEnc.setPassword(key);
+    return pbeEnc.encrypt(value);
+  }
 
 }
