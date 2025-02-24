eabb25eee546ebfd17d232fb73e5919266e24208
tony <dohyun0047@gmail.com>
2025-02-24 21:28:10 +0900

    multi platform docker build

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ echo "Running gradlew bootjar "
 
 # Step 2: Build the Docker image
 echo "Building Docker image..."
-docker build -t auth-service .
+docker buildx build --platform linux/amd64,linux/arm64 --load -t auth-service .
 
 # Step 3: Tag the Docker image
 echo "Tagging Docker image..."
